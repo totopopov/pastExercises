6e17d5a1af158f7a0bd203d0380691a9f5c430bf
totopopov <totopopov@gmail.com>
2016-09-20 15:27:50 +0300

    testing branching

--- Exam/Exam1/Program.cs
+++ Exam/Exam1/Program.cs
@@ -20,7 +20,7 @@ namespace Exam1
 
             Console.WriteLine("{0:f2}",(workdays*money*14.5*0.75*kurs)/365); //
  
-Conflict resolved, peace is restored. now world war 3. Tradegy avoided. 
+Conflict resolved, peace is restored. now world war 3. Tradegy avoided. branch testing.
         }
     }
 }
